40fe1542d85bb1ea75b813e7b528ccd79308ed63
LinoyBoni <linoyboni@gmail.com>
2024-07-27 21:15:35 +0300

    try

--- vite-project/src/Top.jsx
+++ vite-project/src/Top.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';/////
 import axios from 'axios';////
 
 export default function Top() {
-  
+   
   const CarFilter = () => {
       const [company, setCompany] = useState('');
       const [price, setPrice] = useState('');
